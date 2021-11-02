af0fae3aa6dceac43b37cc0da79202c110996cd0
dokkalemon <dottori.roberto@gmail..com>
2021-11-02 15:44:54 +0100

    add difficult grid rules

--- js/main.js
+++ js/main.js
@@ -7,12 +7,59 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
 
-// Referenza della difficoltà, del bottone e della griglia
+// Referenze della difficoltà, del bottone e della griglia
 const selectRef = document.getElementById('difficult');
 const playBtn = document.querySelector('.btn');
-const grid = document.querySelector('.grid');
+const gridConteiner = document.querySelector('.grid-conteiner');
 
 playBtn.addEventListener('click', function() {
     console.log('click me')
 })
 
+// Difficoltà
+selectRef.addEventListener('click', () => {
+
+    //Resettiamo il conteiner
+    gridConteiner.innerHTML = '';
+
+    //Settiamo il grid in base alla difficoltà
+    const difficultGrid = selectRef.value;
+    let numberCells;
+    let cellPerSide;
+
+    switch (difficultGrid) {
+        case '1':
+            numberCells = 49;
+            cellPerSide = 7;
+            break;
+        case '2':
+            numberCells = 81;
+            cellPerSide = 9;
+            break;
+        case '3':
+            numberCells = 100;
+            cellPerSide = 10;
+    }
+            console.log(numberCells);
+            console.log(cellPerSide);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* FUNCTIONS */
+
+//Generiamo una square
+function createSquare() {
+    document.createElement(div);
+
+}
